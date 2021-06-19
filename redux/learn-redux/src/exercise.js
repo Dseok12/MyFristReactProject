f549219ae1961976cd19f762d7502fa99a886c4c
D.seok <ehskstkrjsa@naver.com>
2021-06-19 19:02:01 +0900

    ADD FILE: exercise.js

---
+++ redux/learn-redux/src/exercise.js
@@ -0,0 +1,33 @@
+import { createStore } from 'redux';
+
+const initialState = { 
+    counter: 0,
+    text: '',
+    list: []
+ };
+
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+const CHANGE_TEXT = 'CHANGE_TEXT';
+const ADD_TO_LIST = 'ADD_TO_LIST';
+
+// function increase (){
+//     return {
+//         type: INCREASE,
+
+//     }
+// }
+function increase = () => {
+    type: INCREASE,
+}
+
+
+
+
+
+
+
+
+
+
+

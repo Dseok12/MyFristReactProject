065f645490f225fde237de79a5c9d5de17b86931
D.seok <ehskstkrjsa@naver.com>
2021-06-24 09:43:13 +0900

    study: redux basic

--- redux/learn-redux/src/exercise.js
+++ redux/learn-redux/src/exercise.js
@@ -11,15 +11,75 @@ const DECREASE = 'DECREASE';
 const CHANGE_TEXT = 'CHANGE_TEXT';
 const ADD_TO_LIST = 'ADD_TO_LIST';
 
-// function increase (){
-//     return {
-//         type: INCREASE,
+/* const increase (){
+    return {
+        type: INCREASE,
 
-//     }
-// }
-function increase = () => {
+    }
+} */
+const increase = () => ({
     type: INCREASE,
+});
+const decrease = () => ({
+    type: DECREASE,
+});
+
+const changeText = text => ({
+    type: CHANGE_TEXT,
+    text
+});
+
+const addToList = item => ({
+    type: ADD_TO_LIST,
+    item
+});
+
+function reducer ( state = initialState, action ){
+    switch (action.type) {
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + 1,
+            };
+        case DECREASE:
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        case CHANGE_TEXT:
+            return {
+                ...state,
+                text: action.text
+            }
+        case ADD_TO_LIST:
+            return {
+                ...state,
+                list: state.list.concat(action.item)
+            };
+        default:
+            return state;
+    }
 }
+
+const store = createStore(reducer);
+console.log(store.getState());
+
+const listener = () => {
+    const state = store.getState();
+    console.log(state);
+};
+
+const unsubscribe = store.subscribe(listener);
+// unsubscribe();
+
+store.dispatch(increase());
+store.dispatch(decrease());
+store.dispatch(changeText('안녕하세요'));
+store.dispatch(addToList({ id: 1, text: '와우' }));
+
+window.store = store;
+window.unsubscribe = unsubscribe;
+
 
 
 
